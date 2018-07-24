e3cf095f8565fb3a91941a12948078036b5ac1bb
PrinceCrafter <35907951+PrinceCrafter@users.noreply.github.com>
2018-07-24 20:09:55 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
  const prefix = ">";
 client.on('ready', () => {
-     client.user.setActivity(" >help | Users [${client.users.size}]",{type: 'WATCHING'})
+     client.user.setActivity(" >help | Users ${client.users.size}",{type: 'WATCHING'})
 
 });
 
