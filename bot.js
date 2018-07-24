3e2032adb123356260a99dc6e5b8c3b77c3e1dd8
PrinceCrafter <35907951+PrinceCrafter@users.noreply.github.com>
2018-07-24 19:51:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -804,8 +804,8 @@ client.on('message', message => {
         let embed = new Discord.RichEmbed()
         .setAuthor(` ${message.author.username} `, message.author.avatarURL)      
         .setTitle(`:small_orange_diamond: اضغط هنا `)
-        .setURL(`https://discordapp.com/oauth2/authorize?client_id=470331722060529664&permissions=8&scope=bot`)
-        .setThumbnail(" https://discordapp.com/oauth2/authorize?client_id=471366466344583169&permissions=8&scope=bot")        
+        .setURL(`https://discordapp.com/oauth2/authorize?client_id=471366466344583169&permissions=8&scope=bot`)
+        .setThumbnail(" https://cdn.discordapp.com/avatars/377904849783750667/6c76e412f18c142dfd711d05fb363869.png?size=2048")        
      message.channel.sendEmbed(embed);
        }
    });
